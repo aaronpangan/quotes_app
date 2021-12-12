c77225d4f49277f7763d190c2409053c99d60e20
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-12-12 17:00:52 +0800

    Add zenquotes api

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:dio/dio.dart';
 
-void main() {}
+void main() {
+  runApp(new MaterialApp(home: Quotes()));
+}
+
+class Quotes extends StatefulWidget {
+  @override
+  _QuotesState createState() => _QuotesState();
+}
+
+class _QuotesState extends State<Quotes> {
+  String quotes = "Click the button to generate quotes";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Zen Quotes Generator"),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(quotes),
+            TextButton.icon(
+                onPressed: _generateQuote,
+                icon: Icon(Icons.wb_sunny),
+                label: Text("Generate Quote"))
+          ],
+        ),
+      ),
+    );
+  }
+
+  _generateQuote() async {
+    var response = await Dio().get("https://zenquotes.io/api/random");
+    String quote = response.data.toString().split('"')[3];
+    String author = response.data.toString().split('"')[7];
+    setState(() {
+      quotes = "${quote}\n-${author}";
+    });
+
+    print(author);
+  }
+}
